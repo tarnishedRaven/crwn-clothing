7f65555b3449b991dc8a3b74064bba56b28bcbfd
Adam Brady <thetarnishedraven@gmail.com>
2022-02-12 07:04:50 -0700

    adding shop page, collection preview component and collection item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,49 +2,14 @@ import React from "react";
 
 import MenuItem from "../menu-item/menu-item.component";
 import "./directory.styles.scss";
+import SECTIONS_DATA from "./sections.data";
 
 class Directory extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      sections: [
-        {
-          title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          size: "medium",
-          id: 1,
-          linkUrl: "shop/hats",
-        },
-        {
-          title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          size: "medium",
-          id: 2,
-          linkUrl: "shop/jackets",
-        },
-        {
-          title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          size: "medium",
-          id: 3,
-          linkUrl: "shop/sneakers",
-        },
-        {
-          title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-          size: "large",
-          id: 4,
-          linkUrl: "shop/womens",
-        },
-        {
-          title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-          size: "large",
-          id: 5,
-          linkUrl: "shop/mens",
-        },
-      ],
+      sections: SECTIONS_DATA,
     };
   }
 
@@ -60,3 +25,41 @@ class Directory extends React.Component {
 }
 
 export default Directory;
+
+// [
+//   {
+//     title: "hats",
+//     imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+//     size: "medium",
+//     id: 1,
+//     linkUrl: "shop/hats",
+//   },
+//   {
+//     title: "jackets",
+//     imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+//     size: "medium",
+//     id: 2,
+//     linkUrl: "shop/jackets",
+//   },
+//   {
+//     title: "sneakers",
+//     imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+//     size: "medium",
+//     id: 3,
+//     linkUrl: "shop/sneakers",
+//   },
+//   {
+//     title: "womens",
+//     imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+//     size: "large",
+//     id: 4,
+//     linkUrl: "shop/womens",
+//   },
+//   {
+//     title: "mens",
+//     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+//     size: "large",
+//     id: 5,
+//     linkUrl: "shop/mens",
+//   },
+// ],
